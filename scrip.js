bc47805de1fa68896f3c929badd445e65c597e92
raza@bjmdigital.com.au <raza.atakiu@gmail.com>
2023-06-14 22:54:20 +0500

    adding search option and udpate css

--- scrip.js
+++ scrip.js
@@ -1,16 +1,19 @@
-// Get all the checkboxes
+const resetAllButton = document.querySelector("button");
 const checkboxes = document.querySelectorAll(".checkbox-input");
 
 // Load the checkbox state from localStorage
 for (let i = 0; i < checkboxes.length; i++) {
   const checkbox = checkboxes[i];
+  const list = checkbox.closest("li");
   const checkboxId = checkbox.getAttribute("id");
   const savedState = localStorage.getItem(checkboxId);
 
   if (savedState === "checked") {
     checkbox.checked = true;
+    list.classList.add("grey-bg");
   } else {
     checkbox.checked = false;
+    list.classList.remove("grey-bg");
   }
 }
 
@@ -18,11 +21,38 @@ for (let i = 0; i < checkboxes.length; i++) {
 checkboxes.forEach(function (checkbox) {
   checkbox.addEventListener("change", function () {
     const checkboxId = checkbox.getAttribute("id");
+    const li = checkbox.closest("li");
 
     if (checkbox.checked) {
+      li.classList.add("grey-bg");
       localStorage.setItem(checkboxId, "checked");
     } else {
+      li.classList.remove("grey-bg");
       localStorage.setItem(checkboxId, "unchecked");
+    }
+  });
+});
+
+resetAllButton.addEventListener("click", function () {
+  localStorage.clear();
+  window.location.reload();
+});
+
+
+window.addEventListener("load", function () {
+  let filter = document.getElementById("searchBox");
+
+  filter.addEventListener("keyup", function () {
+    var search = this.value.toLowerCase();
+    var items = document.querySelectorAll("ul li");
+
+    for (let item of items) {
+      let singleItem = item.innerHTML.toLowerCase();
+      if (singleItem.indexOf(search) == -1) {
+        item.classList.add("hide");
+      } else {
+        item.classList.remove("hide");
+      }
     }
   });
 });
